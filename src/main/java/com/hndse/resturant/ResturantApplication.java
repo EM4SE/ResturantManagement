75eba509db1dd0759fbc5179bdf90140ea7e7493
Anjana Ekanayaka <anjana2966ekanayaka@gmail.com>
2025-01-27 20:07:52 +0530

    done

--- src/main/java/com/hndse/resturant/ResturantApplication.java
+++ src/main/java/com/hndse/resturant/ResturantApplication.java
@@ -1,9 +1,12 @@
 package com.hndse.resturant;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Resturant API", version = "1.0"))
 public class ResturantApplication {
 
 	public static void main(String[] args) {
